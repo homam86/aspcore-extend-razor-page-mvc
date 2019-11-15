b4357d0636399833eab4bfc8ee6d8d124f542c51
Houmam <houmam.wazzeh@gmail.com>
2019-11-16 00:28:57 +0300

    master: Initial ASP>NET COre MVC Web Application

---
+++ WebApplication1/Models/HelloModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace WebApplication1.Models
+{
+    public class HelloModel
+    {
+        public string Name { get; set; }
+
+        public HelloModel(string name)
+        {
+            Name = name;
+        }
+    }
+}
---
+++ WebApplication1/Razor/CustomRazorPage.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WebApplication1.Razor
+{
+public abstract class CustomRazorPage<TModel> : Microsoft.AspNetCore.Mvc.Razor.RazorPage<TModel>
+{
+    public string SayHi(string name)
+    {
+        return $"Hi <strong>{name}</strong>";
+    }
+}
+}
